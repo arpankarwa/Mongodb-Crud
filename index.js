309943cf9c2922c99cb6db6be842427a589a272e
ak <arpankarwa@gmail.com>
2023-03-24 15:15:22 +0530

    changes 24/03 3pm

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 
 var express = require('express');
 var app = express();
+app.use(express.json());
 
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
-var port = 4000;
+var port = process.env.PORT || 4000;
 
+mongoose.connect("mongodb://localhost:27017/crudApp", {
+
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+
+    console.log("connection with database is successful");
+}).catch(() => {
+
+    console.log("connection with database is failed....");
+})
+
+// -------------------------------------------------------------------------------------------
 
 app.get('/', (req, res) => {
     console.log("get is working");
     res.send('get is working');
 })
 
+// -------------------------------------------------------------------------------------------
 
 app.listen(port, () => {
     console.log(`app is started on port no ${port}`);
