d0e6df12f1e977671dcf1fcd2f83df8a091b9df6
ak <arpankarwa@gmail.com>
2023-03-24 17:23:47 +0530

    changes 24/03 5pm

--- index.js
+++ index.js
@@ -20,11 +20,89 @@ mongoose.connect("mongodb://localhost:27017/crudApp", {
     console.log("connection with database is failed....");
 })
 
+const employeeSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    designation: String,
+    phone: Number
+})
+
+const Employee = new mongoose.model("Employee", employeeSchema);
+
 // -------------------------------------------------------------------------------------------
 
 app.get('/', (req, res) => {
     console.log("get is working");
     res.send('get is working');
+})
+
+// -------------------------------------------------------------------------------------------
+
+app.post('/insert', async (req, res) => {
+
+    const dataInsert = new Employee({
+        name: req.body.name,
+        email: req.body.email,
+        designation: req.body.designation,
+        phone: req.body.phone
+    });
+
+    const values = await dataInsert.save();
+    res.send("data inserted successfully..\n" + values);
+    // res.send(values.json());
+
+})
+
+// -------------------------------------------------------------------------------------------
+
+//updating data
+app.put('/update/:email', async (req, res) => {
+
+    let newEmail = req.params.email;
+    let newName = req.body.name;
+    let newDesignation = req.body.designation;
+    let newPhone = req.body.phone;
+
+    Employee.findOneAndUpdate({ email: newEmail },
+        { $set: { name: newName, designation: newDesignation, phone: newPhone } },
+        { new: true },
+
+    ).then((data) => {
+        console.log("data updated\n" + data);
+        res.send("data updated\n" + data);
+    }).catch((err) => {
+        console.log("error generated:- " + err);
+        res.send("error generated\n" + err);
+    })
+})
+
+// -------------------------------------------------------------------------------------------
+
+app.get('/getAllEmployees', async (req, res) => {
+
+    Employee.find().then(() => {
+        console.log("all employees fetched\n" + res);
+        // res.send("all employees fetched\n" + res);
+        res.send("all employees fetched\n" + res);
+    }).catch((err) => {
+        console.log("error generated:- " + err);
+        res.send("error generated\n" + err);
+    })
+})
+
+// -------------------------------------------------------------------------------------------
+
+app.delete('/deleteOne/:email', async (req, res) => {
+
+    let newEmail = req.params.email;
+
+    Employee.deleteOne({ email: newEmail }).then(() => {
+        console.log("record deleted\n" + newEmail);
+        res.send("record deleted\n" + newEmail);
+    }).catch((err) => {
+        console.log("error generated:- " + err);
+        res.send("error generated\n" + err);
+    })
 })
 
 // -------------------------------------------------------------------------------------------
